8f330c058d54df346602831520686448c1f76425
onestar99 <onestar99@gmail.com>
2024-02-21 10:35:50 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <MainSidebar></MainSidebar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MainSidebar from './components/layout/MainSidebar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainSidebar
+  }
+}
+</script>
+
+<style>
+
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  background-color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ src/components/layout/MainSidebar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main-sidebar">
+      개발자 정한별
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MainSidebar',
+    // 컴포넌트 로직
+  };
+  </script>
+  
+  <style scoped>
+  .main-sidebar {
+    margin: 0;
+    height: 100%;
+    width: 350px;
+    background-image: url('~@/assets/images/space-sidebar.jpg');
+    background-size: cover; /* 배경 이미지가 div를 꽉 채우도록 설정 */
+    background-position: center; /* 이미지가 중앙에 오도록 설정 */
+
+  }
+  </style>
+  
